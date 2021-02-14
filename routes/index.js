1d28e5026868cb205e84cde6de2efedce8fbe2ab
jithinjohnkaniyampoikayil <jithin.john537@gmail.com>
2021-02-14 00:16:24 +0000

    path updated

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendfile(__dirname+'/dist/index.html')
+  res.sendfile('./dist/index.html')
 });
 router.get("/url", (req, res, next) => {
  res.json(["Tony","Lisa","Michael","Ginger","Food"]);
