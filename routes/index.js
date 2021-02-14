4713196db3b72842b7857c6bb61bcf84b1e60ed0
jithinjohnkaniyampoikayil <jithin.john537@gmail.com>
2021-02-14 00:05:17 +0000

    setting angular and node

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendfile(__dirname+'/dist/index.html')
 });
 router.get("/url", (req, res, next) => {
  res.json(["Tony","Lisa","Michael","Ginger","Food"]);
